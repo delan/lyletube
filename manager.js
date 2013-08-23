5493e9da47304e1f7ae89f89e34b77d921a12738
Delan Azabani <delan@azabani.com>
2013-08-23 23:03:36 +0800

    implemented basic manager skeleton

--- manager.js
+++ manager.js
@@ -1,16 +1,26 @@
 (function(global) {
 
-var player = window.open(
-	'player.html', 'player',
-	'width=640,height=360,scrollbars=0,' +
-	'menubar=0,toolbar=0,location=0,' +
-	'personalbar=0,directories=0,status=0'
-);
+var player;
+
+function reopen() {
+	if (player && !player.closed)
+		player.close();
+	player = window.open(
+		'player.html', 'player',
+		'width=640,height=360,scrollbars=0,' +
+		'menubar=0,toolbar=0,location=0,' +
+		'personalbar=0,directories=0,status=0'
+	);
+	global.player = player;
+}
 
 $(window).on('unload', function() {
-	player.close();
+	if (player)
+		player.close();
 });
 
-global.player = player;
+$('#reopen').click(reopen);
+
+reopen();
 
 })(this);
