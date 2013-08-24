46c6e86b2ab65fa77938ef21bf8a216b80a5cc49
Delan Azabani <delan@azabani.com>
2013-08-24 20:37:35 +0800

    reimplement commit eb89e746: disable mute and set full volume

--- static/manager.js
+++ static/manager.js
@@ -105,6 +105,8 @@ function play_video(id) {
 	if (ready) {
 		player_window.$('#youtube_element').css('opacity', 1);
 		player_window.yt.loadVideoById(id);
+		yt.unMute();
+		yt.setVolume(100);
 	}
 }
 
