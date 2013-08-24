0585083d0fc5a104aa4e04d2f8da2197236ce617
Delan Azabani <delan@azabani.com>
2013-08-24 10:50:28 +0800

    add basic flask/pyinstaller structure from lookout

---
+++ lyletube.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import flask, os, sys, time, json
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
+if getattr(sys, 'frozen', None):
+	basedir = sys._MEIPASS
+else:
+	basedir = os.path.dirname(__file__)
+
+app = flask.Flask(__name__, static_folder=os.path.join(basedir, 'static'))
+PORT = 80
+
+@app.route('/')
+def hello():
+	return flask.send_from_directory(app.static_folder, 'submit.html')
+
+@app.route('/manager')
+def manager():
+	return flask.send_from_directory(app.static_folder, 'manager.html')
+
+@app.route('/player')
+def player():
+	return flask.send_from_directory(app.static_folder, 'player.html')
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		PORT = sys.argv[1]
+	server = HTTPServer(WSGIContainer(app))
+	print 'Now listening on port ' + str(PORT)
+	server.listen(PORT)
+	IOLoop.instance().start()
--- static/manager.js
+++ static/manager.js
@@ -6,7 +6,7 @@ function reopen() {
 	if (player && !player.closed)
 		player.close();
 	player = window.open(
-		'player.html', 'player',
+		'player', 'player',
 		'width=640,height=360,scrollbars=0,' +
 		'menubar=0,toolbar=0,location=0,' +
 		'personalbar=0,directories=0,status=0'
